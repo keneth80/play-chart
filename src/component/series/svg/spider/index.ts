650a031c3a476bc4cc5738a9703929cf3474203a
Kenneth <pretty9967@naver.com>
2023-03-09 13:23:56 +0900

    modify spider chart

--- src/component/series/svg/spider/index.ts
+++ src/component/series/svg/spider/index.ts
@@ -7,6 +7,7 @@ import {ContainerSize, Scale} from '../../../../component/chart/chart.interface'
 import {SeriesBase} from '../../../../component/chart/series-base';
 import {SeriesConfiguration} from '../../../../component/chart/series.interface';
 import {defaultChartColors} from '../../../../component/chart/util/chart-util';
+import {getTransformByArray} from '../../../../component/chart/util';
 
 interface DataPosition {
     x: number;
@@ -70,7 +71,6 @@ export class SpiderSeries extends SeriesBase {
         const height = width;
         const radialScale = scaleLinear().domain(this.domain).range([0, 150]);
         const ticks = radialScale.ticks(this.tickCount);
-        console.log('radialScale : ', chartData, geometry, this.features, this.domain);
         const guideLine: Array<SpiderData> = [];
 
         for (let i = 0; i < ticks.length; i++) {
@@ -78,6 +78,8 @@ export class SpiderSeries extends SeriesBase {
             this.features.forEach((f) => (point[f] = ticks[i]));
             guideLine.push(point);
         }
+        const mainTransform = getTransformByArray(this.mainGroup.attr('transform'));
+        this.mainGroup.attr('transform', `translate(${geometry.width / 2 - width / 2}, ${mainTransform[1]})`);
 
         // draw tick labels
         this.mainGroup
@@ -97,8 +99,13 @@ export class SpiderSeries extends SeriesBase {
             return {
                 name: f,
                 angle: angle,
-                lineValue: angleToCoordinate(angle, radialScale(10), width, height),
-                labelValue: angleToCoordinate(angle, radialScale(11), width, height)
+                lineValue: angleToCoordinate(angle, radialScale(ticks[ticks.length - 1]), width, height),
+                labelValue: angleToCoordinate(
+                    angle,
+                    radialScale(ticks[ticks.length - 1]) + radialScale(ticks[ticks.length - 1] * 0.2),
+                    width,
+                    height
+                )
             };
         });
 
@@ -164,7 +171,15 @@ export class SpiderSeries extends SeriesBase {
                     enter
                         .append('text')
                         .attr('class', 'axis-label')
-                        .style('text-anchor', 'middle')
+                        .style('text-anchor', (d) => {
+                            if (width / 2 === d.labelValue.x) {
+                                return 'middle';
+                            } else if (width / 2 > d.labelValue.x) {
+                                return 'end';
+                            } else {
+                                return 'start';
+                            }
+                        })
                         .attr('x', (d) => d.labelValue.x)
                         .attr('y', (d) => d.labelValue.y)
                         .text((d) => (this.labelFmt ? this.labelFmt(d.name) : d.name))
@@ -221,13 +236,19 @@ function getAngle(index: number, featuresLength: number): number {
     return Math.PI / 2 - (2 * Math.PI * index) / featuresLength;
 }
 
-function angleToCoordinate(angle: number, value: number, width: number, height: number) {
+function angleToCoordinate(angle: number, value: number, width: number, height: number): DataPosition {
     let x = Math.cos(angle) * value;
     let y = Math.sin(angle) * value;
     return {x: width / 2 + x, y: height / 2 - y};
 }
 
-function getPathCoordinates(dataPoint: SpiderData, features: Array<string>, width: number, height: number, radialScale: any) {
+function getPathCoordinates(
+    dataPoint: SpiderData,
+    features: Array<string>,
+    width: number,
+    height: number,
+    radialScale: any
+): Array<DataPosition> {
     const coordinates = [];
     for (let i = 0; i < features.length; i++) {
         const angle = getAngle(i, features.length);
