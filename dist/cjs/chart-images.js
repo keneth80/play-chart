a2789f8be7211a469512d4add480e7cfa2b0107c
강효선/모바일개발팀 <hseonkang@golfzon.com>
2023-04-27 13:28:40 +0900

    added image module

---
+++ dist/cjs/chart-images.js
@@ -0,0 +1,7 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.blueImage = void 0;
+var tslib_1 = require("tslib");
+var blue_angular_png_1 = tslib_1.__importDefault(require("./assets/image/blue_angular.png"));
+exports.blueImage = blue_angular_png_1.default;
+//# sourceMappingURL=chart-images.js.map
---
+++ dist/mjs/chart-images.js
@@ -0,0 +1,3 @@
+import blueImage from './assets/image/blue_angular.png';
+export { blueImage };
+//# sourceMappingURL=chart-images.js.map
