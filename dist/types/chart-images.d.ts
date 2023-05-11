afec2b3c435eea9bf16ddf7ad8aac671cc79e25b
강효선/모바일개발팀 <hseonkang@golfzon.com>
2023-05-11 13:15:33 +0900

    added image

--- dist/types/chart-images.d.ts
+++ dist/types/chart-images.d.ts
@@ -1,5 +1,6 @@
 import blueImage from './assets/image/blue_angular.png';
 import greenImage from './assets/image/green_angular.png';
 import spiderGuide from './assets/image/spider_guide.png';
-export { blueImage, greenImage, spiderGuide };
+import spiderGuideOpacity from './assets/image/spider_guide_opacity.png';
+export { blueImage, greenImage, spiderGuide, spiderGuideOpacity };
 //# sourceMappingURL=chart-images.d.ts.map
